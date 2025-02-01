301d63444c7e4a17105f2adc6c166546a1596198
Alexander <sanekmrd789@gmail.com>
2025-02-01 23:19:20 +0300

    add cancel button in cancel menu

--- src/menu.py
+++ src/menu.py
@@ -5,6 +5,8 @@ button_post = KeyboardButton(text="Опубликовать пост")
 button_contact = KeyboardButton(text="Связь с проектом")
 button_volunteer = KeyboardButton(text="Стать волонтёром/психологом")
 
+button_cancel = KeyboardButton(text="Отмена")
+
 # Создаем клавиатуру
 main_menu = ReplyKeyboardMarkup(
     keyboard=[
@@ -13,4 +15,12 @@ main_menu = ReplyKeyboardMarkup(
         [button_volunteer],  # Третий ряд с одной кнопкой
     ],
     resize_keyboard=True,  # Автоматическое изменение размера клавиатуры
+)
+
+# Создаем меню с кнопкой "Отмена"
+cancel_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [button_cancel],  # Кнопка "Отмена"
+    ],
+    resize_keyboard=True,
 )
